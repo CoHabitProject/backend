efc12f45be383aea126b354d3aef0421d5239daa
ChineDmitri <mr.dima11@gmail.com>
2025-04-26 17:19:24 +0200

    init back

---
+++ co-habit-project/co-habit-application/src/main/java/fr/cohabit/CoHabitApplication.java
@@ -0,0 +1,11 @@
+package fr.cohabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class CoHabitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoHabitApplication.class, args);
+    }
+}
