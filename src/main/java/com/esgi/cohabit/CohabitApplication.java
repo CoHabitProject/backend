1736cdda7979312c2247fec1428a02cc1f6d8b75
Bertrand2808 <bertrand2808@icloud.com>
2025-04-26 16:06:05 +0200

    first commit

---
+++ src/main/java/com/esgi/cohabit/CohabitApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.cohabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohabitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esgi/cohabit/CohabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.cohabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
