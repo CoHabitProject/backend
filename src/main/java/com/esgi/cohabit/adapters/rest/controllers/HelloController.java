8f29a9760a5dee245008b98283bd725851b55c2e
Bertrand2808 <bertrand2808@icloud.com>
2025-04-27 12:51:45 +0200

    set up monitoring and docker

---
+++ src/main/java/com/esgi/cohabit/adapters/rest/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.esgi.cohabit.adapters.rest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
