8f9c0f54d044a2ffddacaf1d6831b32a75572aec
ChineDmitri <mr.dima11@gmail.com>
2025-07-04 08:42:03 +0200

    Fix annotation for differents modules

--- co-habit-project/co-habit-rest/src/main/java/fr/esgi/rest/CoHabitRestApplication.java
+++ co-habit-project/co-habit-rest/src/main/java/fr/esgi/rest/CoHabitRestApplication.java
@@ -2,9 +2,7 @@ package fr.esgi.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -12,11 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "fr.esgi.service",
         "fr.esgi.persistence",
         "fr.esgi.security"
-})
-@EntityScan(basePackages = "fr.esgi.persistence.entity")
-@EnableJpaRepositories(basePackages = {
-        "fr.esgi.persistence.repository.user",
-        "fr.esgi.persistence.repository.space"
 })
 public class CoHabitRestApplication {
 
--- co-habit-project/co-habit-service/src/main/java/fr/esgi/service/CoHabitServiceApplication.java
+++ co-habit-project/co-habit-service/src/main/java/fr/esgi/service/CoHabitServiceApplication.java
@@ -2,10 +2,8 @@ package fr.esgi.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "fr.esgi")
-@EntityScan(basePackages = "fr.esgi.persistence.entity")
 public class CoHabitServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoHabitServiceApplication.class, args);
